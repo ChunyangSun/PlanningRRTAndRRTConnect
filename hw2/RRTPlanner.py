9c5d7cf6287742ebeefcf7d66347cc06acb6254d
Unchapped <nate@nathanielchapman.com>
2014-03-18 21:56:53 -0400

    cleaned up RRT code

--- hw2/RRTPlanner.py
+++ hw2/RRTPlanner.py
@@ -12,64 +12,47 @@ class RRTPlanner(object):
         self.planning_env = planning_env
         self.visualize = visualize
 
+    #  Returns a path should be an array
+    #  of dimension k x n where k is the number of waypoints
+    #  and n is the dimension of the robots configuration space
     def Plan(self, start_config, goal_config, epsilon = 0.01): # epsilon = 0.001
+        #set up visualizer
+        if self.visualize: 
+            self.planning_env.InitializePlot(goal_config)
         
         tree = RRTTree(self.planning_env, start_config)
-
-        plan = []
-        plancopy = []
-        if self.visualize and hasattr(self.planning_env, 'InitializePlot'):
-            self.planning_env.InitializePlot(goal_config)
-        # TODO: Here you will implement the rrt planner
-        #  The return path should be an array
-        #  of dimension k x n where k is the number of waypoints
-        #  and n is the dimension of the robots configuration space - (jane) which is 2 in this case? 
-        
         q_new = start_config
-        sampleGoal = 5
         count = 0
-        dist = 100
+        dist = 9999
+        vid_end = 0
         # if q_new is very near to the goal, break out of the loop 
         # don't worry about goal not included in vertices or edges: start and goal are in path 
         while (dist > epsilon) : 
-            # Pick a sample q_rand in space C, occasionally, sample the goal 
-            if count == sampleGoal:
-                q_rand = goal_config
-                count = 0 
-            else: q_rand = self.planning_env.GenerateRandomConfiguration()
+            # Pick a sample q_rand in space C, sample the goal every 5 cycles 
+            q_rand  = self.planning_env.GenerateRandomConfiguration() if (count % 5) else goal_config
             count += 1
+
             # find q_rand's nearest milestone q in the tree:    
             [vid_start, q_near] = tree.GetNearestVertex(q_rand)             
             # extend a step from q to q_new towards q_rand 
             q_new = self.planning_env.Extend(q_near, q_rand)
-            # add q_new to the tree
-            if q_new != None:
-                vid_end = tree.AddVertex(q_new)
-                tree.AddEdge(vid_start, vid_end)
-                # call whenever an edge is added on the tree 
-                if(type(self.planning_env) is SimpleEnvironment):
-                    self.planning_env.PlotEdge(q_near, q_new) 
-                # get out of the loop if q_new is very near to q_goal
-                dist = self.planning_env.ComputeDistance(q_new, goal_config)    #q_new, goal_config
-        # get the goal ID in edges 
-            goalID = max(tree.edges.keys())
-            keyID = goalID
-        # now q_new is goal, find way back to start 
-	pathlength = 0
-	lastx = 0
-	lasty = 0
-        while (keyID != tree.GetRootId()):
-            # ID in dict edge is indexes of [x,y] position in 2D list vertices
-            valueID = tree.edges[keyID] 
-            plancopy.append(tree.vertices[valueID]) 
-	    pathlength = pathlength + pow(pow((tree.vertices[valueID][0]-lastx), 2) + pow((tree.vertices[valueID][1]-lasty), 2), 0.5)
-            lastx = tree.vertices[valueID][0]
-            lasty = tree.vertices[valueID][1]
-            keyID = valueID
 
-        while (len(plancopy) > 0):
-            plan.append(plancopy.pop())
+            # add q_new to the tree, #TODO: possible improvement, detect/avoid duplicates
+            vid_end = tree.AddVertex(q_new)
+            tree.AddEdge(vid_start, vid_end)
+            # update the distance from the goal
+            dist = self.planning_env.ComputeDistance(q_new, goal_config)
+
+            #visualize the updated tree
+            if self.visualize:
+                self.planning_env.PlotEdge(q_near, q_new) 
+
+        # now q_new is the goal, with index vid_end
+        # retrace the tree to the start to get the plan
+        plan = []
+        currVertex = vid_end
+        while (currVertex != tree.GetRootId()):
+            currVertex = tree.edges[currVertex] 
+            plan.insert(0, tree.vertices[currVertex]) 
         plan.append(goal_config)
-	
-	print pathlength	
         return plan
