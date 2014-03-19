20699e482e27c9a02fd855b62464a43905b5bb52
Unchapped <nate@nathanielchapman.com>
2014-03-18 22:38:20 -0400

    fixed RRTConnect planner (reverse tree wasn't growing...)

--- hw2/RRTConnectPlanner.py
+++ hw2/RRTConnectPlanner.py
@@ -8,84 +8,73 @@ class RRTConnectPlanner(object):
         self.planning_env = planning_env
         self.visualize = visualize
         
-
+    #  RRT Connect Planner Implementation
+    #  returns an array
+    #  of dimension k x n where k is the number of waypoints
+    #  and n is the dimension of the robots configuration space
     def Plan(self, start_config, goal_config, epsilon = 0.001):
-        
+        #set up visualizer
+        if self.visualize: 
+            self.planning_env.InitializePlot(goal_config)
+
         ftree = RRTTree(self.planning_env, start_config)
         rtree = RRTTree(self.planning_env, goal_config)
-        plan = []
-        plancopy = []
-        if self.visualize and hasattr(self.planning_env, 'InitializePlot'):
-            self.planning_env.InitializePlot(goal_config)
-        # TODO: Here you will implement the rrt connect planner
-        #  The return path should be an array
-        #  of dimension k x n where k is the number of waypoints
-        #  and n is the dimension of the robots configuration space
+
         fq_new = start_config
-        sampleGoal = 5
         count = 0
-        dist = 100
+        dist = 9999
 
         while (dist > epsilon) : 
-            # Pick a sample q_rand in space C, occasionally, sample the goal 
-            if count == sampleGoal:
-                q_rand = goal_config
-                count = 0 
-            else: q_rand = self.planning_env.GenerateRandomConfiguration()
+            # Pick a sample q_rand in space C, sample the goal every 5 cycles 
+            q_rand  = self.planning_env.GenerateRandomConfiguration() if (count % 5) else goal_config
             count += 1
-            # find q_rand's nearest milestone q in the tree:    
-            [fvid_start, fq_near] = ftree.GetNearestVertex(q_rand)             
-            # extend a step from q to q_new towards q_rand 
-            fq_new = self.planning_env.Extend(fq_near, q_rand)
-            # add q_new to the tree
-            if fq_new != None:
-                # IPython.embed()
-                # extend node from the reverse tree 
-                [rvid_start, rq_near] = rtree.GetNearestVertex(fq_new)
-                rq_new = self.planning_env.Extend(rq_near, fq_new)
-                # get the front and reverse tree vertex ID, they are independent, both start from root 0 
+
+            # find q_rand's nearest milestone q in both trees:    
+            [fvid_start, fq_near] = ftree.GetNearestVertex(q_rand)   
+            [rvid_start, rq_near] = rtree.GetNearestVertex(q_rand)
+
+            #find the nearest tree
+            if(self.planning_env.ComputeDistance(fq_near, q_rand) < self.planning_env.ComputeDistance(rq_near, q_rand)) or not (count % 5):
+                #attach the node to the forward tree
+                fq_new = self.planning_env.Extend(fq_near, q_rand)
                 fvid_end = ftree.AddVertex(fq_new)
-                rvid_end = rtree.AddVertex(rq_new)
-                # add new edge to front and reverse tree, they are independent 
                 ftree.AddEdge(fvid_start, fvid_end)
+
+                #find the nearest reverse tree node
+                [rvid_end, rq_new] = rtree.GetNearestVertex(fq_new)
+
+                # draw the new edge 
+                if self.visualize: 
+                    self.planning_env.PlotEdge(fq_near, fq_new)
+            else:
+                #attach the node to the reverse tree
+                rq_new = self.planning_env.Extend(rq_near, fq_new)
+                rvid_end = rtree.AddVertex(rq_new)
                 rtree.AddEdge(rvid_start, rvid_end)
-                # call whenever an edge is added on the tree 
-                self.planning_env.PlotEdge(fq_near, fq_new)
-                self.planning_env.PlotEdge(rq_near, rq_new) 
-                # get out of the loop if q_new is very near to q_goal
-                dist = self.planning_env.ComputeDistance(fq_new, rq_new)    
-        # get the goal ID in edges 
-        fkeyID = max(ftree.edges.keys())
-        rkeyID = max(rtree.edges.keys())
-	pathlength = 0
-	lastx = 0
-	lasty = 0
-        # add front-tree path into plancopy
-        while (fkeyID != ftree.GetRootId()):
-            # ID in dict edge is indexes of [x,y] position in 2D list vertices
-            fvalueID = ftree.edges[fkeyID] 
-            plancopy.append(ftree.vertices[fvalueID]) 
-	    pathlength = pathlength + pow(pow((ftree.vertices[fvalueID][0]-lastx), 2) + pow((ftree.vertices[fvalueID][1]-lasty), 2), 0.5)
-            lastx = ftree.vertices[fvalueID][0]
-            lasty = ftree.vertices[fvalueID][1]
-            fkeyID = fvalueID
-        # reverse plancopy, from front tree root to the connecting vertice
-        while (len(plancopy) > 0):
-            plan.append(plancopy.pop())
 
-	lastx = 0
-	lasty = 0
-        # add reverse-tree path into path directly, from connecting vertice to goal
-        while (rkeyID != rtree.GetRootId()):
-            # ID in dict edge is indexes of [x,y] position in 2D list vertices
-            rvalueID = rtree.edges[rkeyID] 
-            plan.append(rtree.vertices[rvalueID]) 
-	    pathlength = pathlength + pow(pow((rtree.vertices[rvalueID][0]-lastx), 2) + pow((rtree.vertices[rvalueID][1]-lasty), 2), 0.5)
-	    lastx = rtree.vertices[rvalueID][0]
-            lasty = rtree.vertices[rvalueID][1]
-	    rkeyID = rvalueID
+                #find the nearest forward tree node
+                [fvid_end, fq_new] = ftree.GetNearestVertex(rq_new)  
 
-        print "points are: ", plan 
-        print "path length is: ", pathlength
+                # draw the new edge 
+                if self.visualize: 
+                    self.planning_env.PlotEdge(rq_near, rq_new) 
 
+            #update the distance metric
+            dist = self.planning_env.ComputeDistance(fq_new, rq_new)
+
+        # final goal IDs are fvid_end, rvid_end
+        plan = []
+
+        # add front-tree path into the plan
+        currVertex = fvid_end
+        while (currVertex != ftree.GetRootId()):
+            currVertex = ftree.edges[currVertex] 
+            plan.insert(0, ftree.vertices[currVertex]) 
+
+        # add reverse-tree into the plan
+        currVertex = rvid_end
+        while (currVertex != rtree.GetRootId()):
+            currVertex = rtree.edges[currVertex ] 
+            plan.append(rtree.vertices[currVertex]) 
+        plan.append(goal_config)
         return plan
--- hw2/run.py
+++ hw2/run.py
@@ -78,8 +78,8 @@ if __name__ == "__main__":
 
     main(robot, planning_env, planner)
 
-    import IPython
-    IPython.embed()
+    #import IPython
+    #IPython.embed()
 
         
     
